9a2df1a272e0d57f1dc176c4c5cc4309c08955c9
Joachim Løvgaard <joachim@loevgaard.dk>
2021-03-16 21:54:58 +0100

    Added more test coverage

--- tests/EventListener/SaveClientIdSubscriberTest.php
+++ tests/EventListener/SaveClientIdSubscriberTest.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
 /**
@@ -37,7 +38,69 @@ final class SaveClientIdSubscriberTest extends TestCase
      */
     public function it_saves(): void
     {
-        $kernel = new class() extends Kernel {
+        $response = new Response();
+
+        $event = new ResponseEvent(self::getKernel(), new Request(), HttpKernelInterface::MASTER_REQUEST, $response);
+
+        $subscriber = new SaveClientIdSubscriber(self::getClientIdProvider(), 'cookie_name');
+        $subscriber->save($event);
+
+        $cookies = $response->headers->getCookies();
+        self::assertCount(1, $cookies);
+
+        $cookie = $cookies[0];
+
+        self::assertSame('cookie_name', $cookie->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_save_cookie_if_request_is_not_a_master_request(): void
+    {
+        $response = new Response();
+
+        $event = new ResponseEvent(self::getKernel(), new Request(), HttpKernelInterface::SUB_REQUEST, $response);
+
+        $subscriber = new SaveClientIdSubscriber(self::getClientIdProvider(), 'cookie_name');
+        $subscriber->save($event);
+
+        self::assertCount(0, $response->headers->getCookies());
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_save_cookie_if_request_is_an_ajax_request(): void
+    {
+        $response = new Response();
+
+        $event = new ResponseEvent(
+            self::getKernel(),
+            new Request([], [], [], [], [], ['HTTP_X-Requested-With' => 'XMLHttpRequest']),
+            HttpKernelInterface::MASTER_REQUEST,
+            $response
+        );
+
+        $subscriber = new SaveClientIdSubscriber(self::getClientIdProvider(), 'cookie_name');
+        $subscriber->save($event);
+
+        self::assertCount(0, $response->headers->getCookies());
+    }
+
+    private static function getClientIdProvider(): ClientIdProviderInterface
+    {
+        return new class() implements ClientIdProviderInterface {
+            public function get(): ClientId
+            {
+                return new ClientId('client_id');
+            }
+        };
+    }
+
+    private static function getKernel(): KernelInterface
+    {
+        return new class() extends Kernel {
             use MicroKernelTrait;
 
             public function __construct()
@@ -62,26 +125,5 @@ final class SaveClientIdSubscriberTest extends TestCase
             {
             }
         };
-
-        $clientIdProvider = new class() implements ClientIdProviderInterface {
-            public function get(): ClientId
-            {
-                return new ClientId('client_id');
-            }
-        };
-
-        $response = new Response('', 301);
-
-        $event = new ResponseEvent($kernel, new Request(), HttpKernelInterface::MASTER_REQUEST, $response);
-
-        $subscriber = new SaveClientIdSubscriber($clientIdProvider, 'cookie_name');
-        $subscriber->save($event);
-
-        $cookies = $response->headers->getCookies();
-        self::assertCount(1, $cookies);
-
-        $cookie = $cookies[0];
-
-        self::assertSame('cookie_name', $cookie->getName());
     }
 }
