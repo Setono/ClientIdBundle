851d797e641d501e8573fcd789641ccb430a23ed
Joachim Løvgaard <joachim@loevgaard.dk>
2021-03-17 13:18:57 +0100

    Add compiler pass

---
+++ src/DependencyInjection/Compiler/RegisterClientIdTypePass.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\ClientIdBundle\DependencyInjection\Compiler;
+
+use Setono\ClientIdBundle\Doctrine\Type\ClientIdType;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class RegisterClientIdTypePass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->hasParameter('doctrine.dbal.connection_factory.types')) {
+            return;
+        }
+
+        /** @var array<string, mixed> $typeDefinition */
+        $typeDefinition = $container->getParameter('doctrine.dbal.connection_factory.types');
+
+        if (!isset($typeDefinition['client_id'])) {
+            $typeDefinition['client_id'] = ['class' => ClientIdType::class];
+        }
+
+        $container->setParameter('doctrine.dbal.connection_factory.types', $typeDefinition);
+    }
+}
--- src/SetonoClientIdBundle.php
+++ src/SetonoClientIdBundle.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Setono\ClientIdBundle;
 
+use Setono\ClientIdBundle\DependencyInjection\Compiler\RegisterClientIdTypePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class SetonoClientIdBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterClientIdTypePass());
+    }
 }
